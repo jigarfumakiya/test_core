a831239ea302d9939e6c74784e646b125e1858a9
Jigar Fumakiya <jigarfumakiya@users.noreply.github.com>
2022-07-23 22:19:47 +0530

    Custom Text and Icon Button.

---
+++ lib/core/widget/app_textfiled.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import '../utils/app_colors.dart';
+import '../utils/app_textstyle.dart';
+import 'icon_button.dart';
+
+typedef onTextChanged = Function(String?);
+
+/// Custom Text TextField
+/// That accepts multiple params
+///  Built in clear text feature
+class AppTextField extends StatefulWidget {
+  final bool showCloseButton;
+  final TextEditingController? controller;
+  final String? hintText;
+  final Icon? icon;
+  final onTextChanged? onChange;
+
+  const AppTextField({
+    Key? key,
+    this.showCloseButton = true,
+    this.controller,
+    this.hintText,
+    this.onChange,
+    this.icon,
+  }) : super(key: key);
+
+  @override
+  State<AppTextField> createState() => _AppTextFieldState();
+}
+
+class _AppTextFieldState extends State<AppTextField> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      onChanged: (value) {
+        setState(() {});
+        if (widget.onChange != null) {
+          widget.onChange!.call(value);
+        }
+      },
+      controller: widget.controller ?? _controller,
+      textAlignVertical: TextAlignVertical.center,
+      cursorColor: AppColors.iconColor,
+      decoration: InputDecoration(
+        suffixIcon: showClearIcon(),
+        hoverColor: AppColors.iconColor,
+        labelStyle: AppTextStyles.blackTextStyle15(),
+        contentPadding: const EdgeInsets.symmetric(vertical: 3, horizontal: 5),
+        icon: widget.icon,
+        border: InputBorder.none,
+        hintText: widget.hintText,
+      ),
+    );
+  }
+
+  Widget showClearIcon() {
+    if ((widget.controller?.text != '' && widget.controller?.text != null) ||
+        _controller.text != '') {
+      if (widget.showCloseButton) {
+        return AppIconButton(
+          onTap: () => onClearTap(),
+          icon: Icons.close,
+        );
+      }
+    }
+    return const SizedBox();
+  }
+
+  void onClearTap() {
+    if (widget.showCloseButton) {
+      if (widget.controller != null) {
+        widget.controller!.clear();
+      } else {
+        _controller.clear();
+      }
+    }
+    setState(() {});
+  }
+}
---
+++ lib/core/widget/icon_button.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import '../utils/app_colors.dart';
+
+class AppIconButton extends StatelessWidget {
+  final IconData? icon;
+  final String? assetPath;
+  final VoidCallback onTap;
+
+  /// App Icon that takes icon data
+
+  const AppIconButton({
+    Key? key,
+    required this.icon,
+    required this.onTap,
+    this.assetPath,
+  })  : assert(icon != null, 'icon data is required'),
+        super(key: key);
+
+  /// App Icon that takes assetPath
+  const AppIconButton.formAsset({
+    Key? key,
+    required this.onTap,
+    required this.assetPath,
+    this.icon,
+  })  : assert(assetPath != null, 'Asset path is required'),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+        primary: Colors.transparent,
+        onPrimary: Colors.black,
+        shadowColor: Colors.white,
+        surfaceTintColor: Colors.white,
+        minimumSize: const Size(0, 0),
+        padding: const EdgeInsets.all(8),
+        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        enableFeedback: true,
+        elevation: 0,
+      ),
+      child: assetPath != null
+          ? Image.asset(
+              assetPath!,
+              width: 25,
+              height: 25,
+              color: AppColors.iconColor,
+            )
+          : Icon(icon, size: 25, color: AppColors.iconColor),
+    );
+  }
+}
