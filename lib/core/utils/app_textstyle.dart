4b970d1bf91443837ff341522c7b21b3f5b6a098
Jigar Fumakiya <jigarfumakiya@users.noreply.github.com>
2022-07-24 15:22:27 +0530

    Added New Text Styles.

--- lib/core/utils/app_textstyle.dart
+++ lib/core/utils/app_textstyle.dart
@@ -30,9 +30,25 @@ class AppTextStyles {
   static TextStyle blackBoldTextStyle14() => GoogleFonts.roboto(
       color: AppColors.iconColor, fontWeight: FontWeight.bold, fontSize: 14.sp);
 
+  static TextStyle blackBoldTextStyle13Light() => GoogleFonts.roboto(
+      color: Colors.black, fontWeight: FontWeight.w300, fontSize: 13.sp);
+
   static TextStyle blackTextStyle15() =>
       GoogleFonts.roboto(color: Colors.black, fontSize: 15.sp);
 
   static TextStyle blackTextStyle14() =>
       GoogleFonts.roboto(color: Colors.black, fontSize: 14.sp);
+
+  static TextStyle blackBoldTextStyle11() => GoogleFonts.roboto(
+      color: Colors.black, fontWeight: FontWeight.w400, fontSize: 11.sp);
+
+  static TextStyle blackBoldTextStyle13() => GoogleFonts.roboto(
+      color: Colors.black, fontWeight: FontWeight.w400, fontSize: 13.sp);
+
+  static TextStyle blackBoldTextStyle13Underline() => GoogleFonts.roboto(
+      letterSpacing: -4,
+      color: Colors.black,
+      fontWeight: FontWeight.w400,
+      decoration: TextDecoration.underline,
+      fontSize: 13.sp);
 }
